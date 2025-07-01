b677593c4135a0e3144012e50f6546c0d8620fa2
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-01 09:52:33 +0530

    For Loops

--- app.py
+++ app.py
@@ -1,28 +1,7 @@
-command = ""
-started = False
-while True:
-    command = input("> ").lower()
-    if command == "start":
-        if started:
-            print("Car is already started!")
-        else:
-            started = True
-            print("Car started...")
-    elif command == "stop":
-        if not started:
-            print("Car is already stopped!")
-        else:
-            started = False
-            print("Car stopped.")
-    elif command == "help":
-        print(
-            """
-start       To start the car
-stop        To stop the car 
-quit        To quit
-        """
-        )
-    elif command == "quit":
-        break
-    else:
-        print("It is not a valid command       try help")
+prices = [10, 20, 30]
+total = 0
+
+for i in prices:
+    total += i
+
+print(f"Total: {total}")
