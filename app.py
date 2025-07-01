53205ccade6b5e040aaf3b377ce770dc5b818b21
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-01 08:47:58 +0530

    Car game

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print("Siddharth Lokhande")
+command = ""
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        print("Car started...")
+    elif command == "stop":
+        print("Car stopped.")
+    elif command == "help":
+        print(
+            """
+start       To start the car
+stop        To stop the car 
+quit        To quit
+        """
+        )
+    elif command == "quit":
+        break
+    else:
+        print("It is not a valid command       try help")
