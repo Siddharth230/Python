6584a90d6c26389be62efe09ba58b0e29a0d4df5
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-03 09:13:47 +0530

    Inheritance

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-class Person:
-    def __init__(self, name):
-        self.name = name
-
-    def talk(self):
-        print(f"Hi, I am {self.name}")
+class Mammal:
+    def walk(self):
+        print("walk")
 
 
-sid = Person("Siddharth Lokhande")
-sid.talk()
+class Dog(Mammal):
+    def bark(self):
+        print("bark")
 
-om = Person("Om")
-om.talk()
+
+class Cat(Mammal):
+    def purr(self):
+        print("purr")
+
+
+dog1 = Dog()
+dog1.walk()
+cat1 = Cat()
+cat1.walk()
