36978f889e4c3c0aae019c5bb731b6c5bb2d2e68
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-03 08:00:03 +0530

    Functions, Parameters, Keyword Arguments

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-message = input(">")
+def greet_user(first_name, last_name):
+  print(f"Hi {first_name} {last_name}!")
+  print("Welcome aboard")
 
-words = message.split(" ")
 
-emojis = {":)": "😀", ":(": "☹️"}
-
-output = ""
-for i in words:
-    output += emojis.get(i, i) + " "
-
-print(output)
+print("Start")
+greet_user("Siddharth", "Lokhande")
+greet_user(last_name="Lokhande", first_name="Om")
+print("Finish")
