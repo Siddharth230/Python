1a35d5195a571b71730c6c40bef831aa57670be0
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-03 08:22:27 +0530

    Reusable Function

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-def greet_user(first_name, last_name):
-  print(f"Hi {first_name} {last_name}!")
-  print("Welcome aboard")
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+        ":)": "😀",
+        ":(": "☹️"
+    }
+    output = ""
+    for i in words:
+        output += emojis.get(i, i) + " "
+    return output
 
 
-print("Start")
-greet_user("Siddharth", "Lokhande")
-greet_user(last_name="Lokhande", first_name="Om")
-print("Finish")
+message = input(">")
+result = emoji_converter(message)
+
+print(result)
