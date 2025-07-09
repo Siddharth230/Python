cbccaad8e1e9e2254cc4558dc9642606a563f075
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-09 11:50:48 +0530

    Variables Revisited Dynamic Typing

--- WEEK 02/main.py
+++ WEEK 02/main.py
@@ -1,23 +1,9 @@
-ram_bank_balance = 100000
-# Ram's bank balance, note that it is positive
+# We are trying to illustrate what we call th dynamic typing
 
-ram_loan = 500000
-# Ram's loan, this is to be returned by him and hence will count as negative
+n = 10
+print(type(n))
+print(n)
 
-lakshman_bank_balance = 2000000
-# Lakshman's bank balance, note that it is positive
-
-lakshman_loan = 1000000
-# Lakshman's loan, this is to be returned by him and hence will count as negative
-
-
-net_income = ram_bank_balance + lakshman_bank_balance
-# It is the total bank balance of the two brother's
-
-net_liability = ram_loan + lakshman_loan
-# It is the total loan of the brother's
-
-final_value = net_income - net_liability
-# Family's final money
-
-print(f"SO the family has {final_value}")
+n = n/2
+print(type(n))
+print(n)
