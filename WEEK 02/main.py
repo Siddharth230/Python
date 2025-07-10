7fd1fda91f095441ab92cc96936e4e880672cd27
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-10 11:32:38 +0530

    Problems on if, else and else-if (elif) conditions

--- WEEK 02/main.py
+++ WEEK 02/main.py
@@ -1,13 +1,61 @@
 
-# * If condition
+# * Problem 1
+number = int(input("Enter a number: "))
 
-birth_year = int(input("Please enter your date of birth: "))
-
-current_year = 2025
-
-age = current_year - birth_year
-
-if age >= 13:
-    print("You can proceed to watch the movie")
+if (number % 2) == 0:
+    print("It is an Even number")
 else:
-    print("You have not met the requirement for minimum required age for this movie")
+    print("It is an Odd number")
+
+
+# * Problem 2
+num = int(input("Enter a number: "))
+
+if (num % 5) == 0:
+    if (num % 10) == 0:
+        print('0')
+    else:
+        print('5')
+else:
+    print("Other")
+
+
+# * Problem 3
+marks = int(input("Enter students marks: "))
+
+if marks < 0 or marks > 100:
+    print('Invalid input: Marks must be between 0 and 100')
+elif marks >= 90:
+    print('A Grade')
+elif marks >= 80:
+    print('B Grade')
+elif marks >= 70:
+    print('C Grade')
+elif marks >= 60:
+    print('D Grade')
+else:
+    print('E Grade')
+
+
+# * Problem 4
+time = int(input('Enter time: '))
+
+longer = int(input('Define longer: '))
+
+if time >= longer:
+    price = int(input('Enter price: '))
+    higher = int(input('Enter higher: '))
+    if price >= higher:
+        print('Train')
+    else:
+        print('Coach')
+else:
+    price = int(input('Enter price: '))
+    higher = int(input('Enter higher: '))
+    if price >= higher:
+        print('Daytime flight')
+    else:
+        print('Red Eye Flight')
+
+
+print('Arrive City B')
