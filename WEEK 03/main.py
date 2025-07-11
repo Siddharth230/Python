8b120807da39d481685efef4d348067b0ea3b112
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-11 09:45:45 +0530

    Introduction to While Loop

---
+++ WEEK 03/main.py
@@ -0,0 +1,6 @@
+year = int(input("When did India get it's independence (year)? "))
+
+while year != 1947:
+    year = int(input("You have got this wrong. Enter once again: "))
+
+print("It's Correct.")
