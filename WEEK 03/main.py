6533d71fa1ae3d4752d6a18f537072a1728017d2
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-11 12:51:09 +0530

    Tutorial on While loop

--- WEEK 03/main.py
+++ WEEK 03/main.py
@@ -1,16 +1,87 @@
 
-# * Let us find the factorial of a number
+# # * Problem 1
 
-n = int(input("Enter a number: "))
+# #? First Approach
+# n = int(input("Enter a number: "))
 
-i = 1
+# if n < 0:
+#     print("Not Defined.")
+# else:
+#     i = 1
+#     fact = 1
+#     while i <= n:
+#         fact *= i
+#         i += 1
+#     print(fact)
 
-ans = 1
+# # ? Second Approach
 
-# ? Executes the loop when condition is True and stops executing when condition becomes False
-while i <= n:
-    ans *= i
-    i += 1
+# n = int(input('Enter a number: '))
+# fact = 1
+# if n < 0:
+#     print('Not Defined')
+# else:
+#     while n > 0:
+#         fact *= n
+#         n -= 1
+#     print(fact)
 
 
-print(ans)
+# # * Problem 2
+
+# # ? First Approach
+
+# n = abs(int(input('Enter a number: ')))
+
+# print(len(str(n)))
+
+# # ? Second Approach
+
+# n = abs(int(input('Enter a number: ')))
+# digits = 1
+# while n > 9:
+#     n = n // 10   # ? Removes last digit
+#     digits += 1
+
+# print(digits)
+
+
+# # * Problem 3
+
+# num = int(input('Enter a number: '))
+# absNum = abs(num)
+# rev = absNum % 10  # ? Extracts last digit
+# absNum //= 10  # ? Removes last digit
+
+# while absNum > 0:
+#     r = absNum % 10
+#     absNum //= 10
+#     rev = rev * 10 + r
+
+
+# if num >= 0:
+#     print(rev)
+# else:
+#     print(rev * -1)
+
+
+# * Problem 4
+
+num = int(input('Enter a number: '))
+absNum = abs(num)
+rev = absNum % 10
+absNum //= 10
+
+while absNum > 0:
+    r = absNum % 10
+    absNum //= 10
+    rev = rev * 10 + r
+
+
+if num < 0:
+    rev *= -1
+
+if num == rev:
+    print('Palindrome')
+else:
+    print('Not a Palindrome')
