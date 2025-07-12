edb3648a785a8b1197bc5b9d1aa0d4e0f6853951
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-12 12:07:36 +0530

    Tutorial on For Loop and difference between While Loop and For Loop

--- WEEK 03/main.py
+++ WEEK 03/main.py
@@ -1,27 +1,107 @@
 
-for x in range(10):
-    print(x, end=' ')
+# * Problem 1: Find the factorial of the given number
 
-d, m, y = 10, 5, 2021
+# ? With While Loop
+num = int(input('Enter a number: '))
+fact = 1
+if num < 0:
+    print('Not Defined')
+else:
+    while num > 0:
+        fact *= num
+        num -= 1
+    print(fact)
 
-print("Today's date is", end=' ')
-print(d, m, y, sep='/')
 
-num = int(input())
+# ? With For Loop
+num = int(input('Enter a number: '))
+fact = 1
+if num < 0:
+    print('Not defined.')
+else:
+    for i in range(num, 1, -1):
+        fact *= i
+    print(fact)
 
-for i in range(1, 11):
-    print(num, 'X', i, '=', num*i)
-    print(f"{num} X {i} = {num * i}")
-    print('{0} X {1} = {2}'.format(num, i, num * i))
-    print('%d X %d = %d' % (num, i, num * i))
 
-pi = 22/7
-print(f'Value of PI = {pi:.3f}')
-print('Value of PI = {0:.9f}'.format(pi))
-print('Value of PI = %.0f' % (pi))
+# * Problem 2: Find the number of digits in the given number
 
-print('{0:5d}'.format(1))
-print('{0:5d}'.format(11))
-print('{0:5d}'.format(111))
-print('{0:5d}'.format(1111))
-print('{0:5d}'.format(11111))
+# ? With While Loop
+num = abs(int(input('Enter a number: ')))
+digits = 1
+while num > 10:
+    num //= 10
+    digits += 1
+print(digits)
+
+
+# ? With For Loop
+num = abs(int(input('Enter a number: ')))
+strNum = str(num)
+digits = 0
+for c in strNum:
+    digits += 1
+print(digits)
+
+
+# * Problem 3: Reverse the digits in the given number
+
+# ? With While Loop
+num = int(input('Enter a number: '))
+absNum = abs(num)
+rev = absNum % 10
+absNum //= 10
+while absNum > 0:
+    r = absNum % 10
+    absNum //= 10
+    rev = rev * 10 + r
+if num > 0:
+    print(rev)
+else:
+    print(rev*-1)
+
+
+# ? With For Loop
+num = int(input('Enter a number: '))
+absStrNum = str(abs(num))
+rev = ''
+for c in absStrNum:
+    rev = c + rev
+if num >= 0:
+    print(rev)
+else:
+    print('-'+rev)
+
+
+# * Problem 4: Find whether the entered number is palindrome or not
+
+
+# ? With While Loop
+num = int(input('Enter a number: '))
+absNum = abs(num)
+rev = absNum % 10
+absNum //= 10
+while absNum > 0:
+    r = absNum % 10
+    absNum //= 10
+    rev = rev * 10 + r
+if num < 0:
+    rev *= -1
+if rev == num:
+    print('Palindrome')
+else:
+    print('Not a Palindrome')
+
+
+# ? With For Loop
+num = int(input('Enter a number: '))
+absStrNum = str(abs(num))
+rev = ''
+for c in absStrNum:
+    rev = c + rev
+if num < 0:
+    rev = '-' + rev
+if num == int(rev):
+    print('Palindrome')
+else:
+    print('Not a Palindrome')
