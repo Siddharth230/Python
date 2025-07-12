45381dbb125ae0cb70e049198ef8af7eafb2503a
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-12 11:38:28 +0530

    Formatted Printing

--- WEEK 03/main.py
+++ WEEK 03/main.py
@@ -1,7 +1,27 @@
 
-for x in range(9, -5, -2):  # ? range(startpoint, endpoint, steps)
-    print(x)
+for x in range(10):
+    print(x, end=' ')
 
-country = 'India'
-for letter in country:
-    print(letter)
+d, m, y = 10, 5, 2021
+
+print("Today's date is", end=' ')
+print(d, m, y, sep='/')
+
+num = int(input())
+
+for i in range(1, 11):
+    print(num, 'X', i, '=', num*i)
+    print(f"{num} X {i} = {num * i}")
+    print('{0} X {1} = {2}'.format(num, i, num * i))
+    print('%d X %d = %d' % (num, i, num * i))
+
+pi = 22/7
+print(f'Value of PI = {pi:.3f}')
+print('Value of PI = {0:.9f}'.format(pi))
+print('Value of PI = %.0f' % (pi))
+
+print('{0:5d}'.format(1))
+print('{0:5d}'.format(11))
+print('{0:5d}'.format(111))
+print('{0:5d}'.format(1111))
+print('{0:5d}'.format(11111))
