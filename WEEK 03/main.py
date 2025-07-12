694ab5f7fcbeeceec43e042b010147d0cdd17b03
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-12 10:50:57 +0530

    For loop on multiplication tables

--- WEEK 03/main.py
+++ WEEK 03/main.py
@@ -1,12 +1,5 @@
 
-# * Adds the first 10 integers
-
 n = int(input("Enter a number: "))
 
-ans = 0
-
-for i in range(n+1):
-    ans += i
-
-
-print(f"The summation of all the numbers until {n} is {ans}.")
+for i in range(1, n+1):
+    print(f"2 X {i} = {i*2}")
