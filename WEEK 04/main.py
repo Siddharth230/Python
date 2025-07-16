1679447e7af16ff2dbe5520c7027acb026d1dd84
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-16 12:53:49 +0530

    Types of Functions

--- WEEK 04/main.py
+++ WEEK 04/main.py
@@ -1,10 +1,27 @@
-def add(c, a=20, b=30):
-    return (a+b-c)
+
+# * First Category
+print(), input(), len()
+# ? Inbuilt Functions
+# ? These Functions are Part of Python Programming Language
+
+# * Second Category
+log(), sqrt(), random(), randrange(), calender(), month()
+# ? Library Functions
+# ? To use these Functions we need to import specific library
+
+# * Third Category
+upper(), lower(), strip(), count(), index(), replace()
+# ?  String Methods (Functions)
+# ? All these Functions can be executed only using Strings
+
+# * Forth Category
 
 
-print(add(40, 20, 30))
-print(add(a=20, b=30, c=40))
-print(add(40))
-print(add(40, 10))
-print(add(40, 10, 50))
-print(add(40, b=10, a=50))
+def square(x):
+    sqr = x**2
+    return sqr
+
+
+print(square(5))
+
+# ? User Defined Functions
