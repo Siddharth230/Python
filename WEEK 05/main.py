cabb1352cfde8ab3a77c6ac077f83dc23ed11a9c
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-18 07:13:49 +0530

    Sorting using Functions

--- WEEK 05/main.py
+++ WEEK 05/main.py
@@ -1,11 +1,44 @@
-d = {'key': 'value'}  # ? Dictionaries Structure
+# Find out the minimum most element in the list l
+# append that to a new list x
+# remove the minimum from the original list l
 
-d = {0: 0, 1: 1, 2: 4, 3: 9, 4: 16}
-print(d)
+def min_list(l):
+    m = l[0]
+    for i in range(len(l)):
+        if l[i] < m:
+            m = l[i]
+    return m
 
-for key in d:
-    print(key, d[key])
 
-print(d.keys())
-print(d.values())
-print(d.items())
+def obv_sort(l):
+    x = []
+    while len(l) > 0:
+        m = min_list(l)
+        x.append(m)
+        l.remove(m)
+    return x
+
+
+def obvious_sort(l):
+    x = []
+    while len(l) > 0:
+        m = l[0]
+        for i in range(len(l)):
+            if l[i] < m:
+                m = l[i]
+        x.append(m)
+        l.remove(m)
+    return x
+
+
+List = [4, 39, 68, 9, 17, 1, 53, 28]
+
+print(List)
+
+# print(obvious_sort(List))
+print(obv_sort(List))
+
+
+# We just learnt that braking our problem
+# into smaller modules and solving them
+# makes it easy
