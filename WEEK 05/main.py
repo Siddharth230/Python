a9c71ffa99892d9563889653f8b58612ef236dd0
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-18 09:12:25 +0530

    Matrix Multiplication using Functions

--- WEEK 05/main.py
+++ WEEK 05/main.py
@@ -1,47 +1,61 @@
-"""
-This module demonstrates matrix multiplication using nested loops and NumPy.
-"""
+# Initialise C to zero
+# Consider two matrices A and B
+# Find the dot product of two lists
 import numpy
 
-r1 = [1, 2, 3]
-r2 = [4, 5, 6]
-r3 = [7, 8, 9]
 
-s1 = [1, 2, 1]
-s2 = [6, 2, 3]
-s3 = [4, 2, 1]
+def initialize_mat(dim):
+    C = []
+    for i in range(dim):
+        C.append([])
+    for i in range(dim):
+        for j in range(dim):
+            C[i].append(0)
+    return C
 
-A = []
-B = []
-A.append(r1)
-A.append(r2)
-A.append(r3)
 
-B.append(s1)
-B.append(s2)
-B.append(s3)
+def dot_product(u, v):
+    dim = len(u)
+    ans = 0
+    for i in range(dim):
+        ans += u[i] * v[i]
+    return ans
 
-C = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-dim = 3
+def row(M, i):
+    dim = len(M)
+    l = []
+    for k in range(dim):
+        l.append(M[i][k])
+    return l
 
-# ? C[i][j] is the dot product of the i th row of A and j th column of B
 
-print(A)
-print(B)
+def col(M, j):
+    dim = len(M)
+    l = []
+    for k in range(dim):
+        l.append(M[k][j])
+    return l
 
-for i in range(dim):
-    for j in range(dim):
-        for k in range(dim):
-            C[i][j] += A[i][k]*B[k][j]
 
+def mat_mul(A, B):
+    dim = len(A)
+    C = initialize_mat(dim)
+    for i in range(dim):
+        for j in range(dim):
+            C[i][j] += dot_product(row(A, i), col(B, j))
+    return C
+
+
+A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+B = [[38, 2, 42], [1, 0, 34], [94, 73, 23]]
+print(row(A, 0))
+print(col(A, 1))
+
+print(mat_mul(A, B))
+
+A = numpy.asmatrix(A)
+B = numpy.asmatrix(B)
+
+C = A*B
 print(C)
-# C[i][j] = dot product of A[i][...] and B[...][j]
-
-X = numpy.asmatrix(A)
-Y = numpy.asmatrix(B)
-
-print(X)
-print(Y)
-
-print(X*Y)
