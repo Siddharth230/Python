5fe73c96ac569966dcff3d604b855f897ce6326d
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-18 14:42:37 +0530

    Functional Programming (Part2)

--- WEEK 05/main.py
+++ WEEK 05/main.py
@@ -1,15 +1,30 @@
-def square(limit):
-    x = 0
-    while x < limit:
-        yield x*x
-        yield x*x*x
-        x += 1
+a = 100
+b = 20
 
+# if a < b:
+#     small = a
+# else:
+#     small = b
 
-a = square(10)
-print(next(a), next(a))
-print(next(a), next(a))
-print(next(a), next(a))
-print(next(a), next(a))
-print(next(a), next(a))
-print(next(a), next(a))
+small = a if a < b else b
+
+print(small)
+
+a = 5
+
+# while a>0: print(a); a-=1
+while a > 0:
+    print(a)
+    a -= 1
+
+fruits = [
+    'mango', 'apple', 'banana', 'orange', 'pineapple', 'watermelon', 'guava', 'kiwi']
+
+# newList = []
+# for fruit in fruits:
+#     if 'n' in fruit:
+#         newList.append(fruit.capitalize())
+
+newList = [fruit.capitalize() for fruit in fruits if 'n' in fruit]
+
+print(newList)
