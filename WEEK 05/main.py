a5066b8c21b972e3a1fb827882e989c45754ad2e
Siddharth230 <siddharthlokhande04@gmail.com>
2025-07-18 09:26:59 +0530

    Scope of Variables

--- WEEK 05/main.py
+++ WEEK 05/main.py
@@ -1,61 +1,21 @@
-# Initialise C to zero
-# Consider two matrices A and B
-# Find the dot product of two lists
-import numpy
+def myFunction1(x):
+    x *= 2  # ? Local Variable
+    print(f"Value of x in function 1 is {x}")
 
 
-def initialize_mat(dim):
-    C = []
-    for i in range(dim):
-        C.append([])
-    for i in range(dim):
-        for j in range(dim):
-            C[i].append(0)
-    return C
+def myFunction2():
+    global x
+    x *= 3  # ? Used Existing Global Variable
+    print(f"Value of x in function 2 is {x}")
 
 
-def dot_product(u, v):
-    dim = len(u)
-    ans = 0
-    for i in range(dim):
-        ans += u[i] * v[i]
-    return ans
+x = 5  # ? Global Variable
+print(f"Value of x before function call {x}")
 
+myFunction1(x)
 
-def row(M, i):
-    dim = len(M)
-    l = []
-    for k in range(dim):
-        l.append(M[i][k])
-    return l
+print(f"Value of x after function1 call {x}")
 
+myFunction2()
 
-def col(M, j):
-    dim = len(M)
-    l = []
-    for k in range(dim):
-        l.append(M[k][j])
-    return l
-
-
-def mat_mul(A, B):
-    dim = len(A)
-    C = initialize_mat(dim)
-    for i in range(dim):
-        for j in range(dim):
-            C[i][j] += dot_product(row(A, i), col(B, j))
-    return C
-
-
-A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-B = [[38, 2, 42], [1, 0, 34], [94, 73, 23]]
-print(row(A, 0))
-print(col(A, 1))
-
-print(mat_mul(A, B))
-
-A = numpy.asmatrix(A)
-B = numpy.asmatrix(B)
-
-C = A*B
-print(C)
+print(f"Value of x after function2 call {x}")
