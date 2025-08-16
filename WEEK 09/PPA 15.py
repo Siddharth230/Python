dc1119dbbd60354b77d0046ca8f655f4d4b21707
Siddharth230 <siddharthlokhande04@gmail.com>
2025-08-16 11:10:21 +0530

    Added .venv and black

--- WEEK 09/PPA 15.py
+++ WEEK 09/PPA 15.py
@@ -7,13 +7,13 @@ def subset_sum(L, s):
     Return:
         result: bool
     """
-    if s == 0 :
-      return True
-    if not L: 
-      return False
-    if L[0] <=s:
-      if subset_sum(L[1:],s-L[0]):
+    if s == 0:
         return True
+    if not L:
+        return False
+    if L[0] <= s:
+        if subset_sum(L[1:], s - L[0]):
+            return True
     if subset_sum(L[1:], s):
-      return True
+        return True
     return False
