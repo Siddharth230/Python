a4c161c383c77da15ffb0d8cab16a76b24dc9a1d
Siddharth230 <siddharthlokhande04@gmail.com>
2025-08-17 18:18:53 +0530

    Classes and Objects

--- WEEK 10/main.py
+++ WEEK 10/main.py
@@ -1,21 +1,26 @@
-a = int(input())
-b = int(input())
-try:
-    f = open("abc.txt", "r")
-    c = a / b
-    print(c)
-except ZeroDivisionError:
-    print("Invalid input, divisor cannot be zero.")
-except NameError:
-    print("Variable not defined.")
-except FileNotFoundError:
-    print("File Not Found.")
-except:
-    print("Something went wrong.")
-finally:
-    f.close()
-    print("From finally block")
+class Student:
+    roll_no = None
+    name = None
 
 
-if a < 18:
-    raise Exception("You are underage, you cannot vote.")
+s0 = Student()
+s1 = Student()
+
+s0.roll_no = 0
+s0.name = "Siddharth"
+
+print(s0.roll_no, s0.name)
+
+print(s1.roll_no, s1.name)
+
+s2 = Student()
+
+s2.roll_no = 2
+s2.name = "Om"
+
+print(s2.roll_no, s2.name)
+
+s50 = Student()
+s50.name = "Sid"
+
+print(s50.roll_no, s50.name)
