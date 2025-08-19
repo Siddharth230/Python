1f27d51e2e0655972cc5ec6ffecf21ee27f1d6bb
Siddharth230 <siddharthlokhande04@gmail.com>
2025-08-19 19:03:58 +0530

    Pandas Series, DataFrame and more

--- WEEK 11/main.py
+++ WEEK 11/main.py
@@ -1,21 +1,28 @@
-# f = open("scores.csv", "r")
-# scores = f.readlines()[1:]
-# max = 0
-# for record in scores:
-#     fields = record.split(",")
-#     if int(fields[8]) > max:
-#         max = int(fields[8])
-# print(max)
-
 import pandas as pd
 
 scores = pd.read_csv("scores.csv")
-print(scores["Total"].max())
-print(scores["Total"].min())
-print(scores["Total"].mean())
-print(scores["Total"].sum())
-print(scores["Total"].sort_values())
-print(scores["Total"].sort_values(ascending=False))
-# print(scores)
-# print(scores.shape)
-# print(scores.count())
+print(scores, type(scores))
+print(scores["Name"], type(scores["Name"]))
+print(scores.head())
+print(scores.tail())
+print(scores[scores["Name"] == "Siddharth"])
+print(scores[scores["Gender"] == "M"]["Total"].max())
+print(scores[scores["Gender"] == "F"]["Total"].max())
+subject = ["Mathematics", "Physics", "Chemistry"]
+for sub in subject:
+    print(f"Above 85 in {sub}")
+    print(scores[(scores["Gender"] == "F") & (scores[sub] > 85)].shape[0])
+    print(scores[(scores["Gender"] == "M") & (scores[sub] > 85)].shape[0])
+print(scores[scores["Physics"].between(70, 85)].shape[0])
+print(scores[scores["Physics"].between(60, 70)].shape[0])
+print(scores[scores["Physics"] < 60].shape[0])
+for sub in subject:
+    print(f"Above average {sub}")
+    avg = scores[sub].mean()
+    print(scores[(scores["Gender"] == "F") & (scores[sub] > avg)].shape[0])
+    print(scores[(scores["Gender"] == "M") & (scores[sub] > avg)].shape[0])
+print(scores.groupby("Gender").groups)
+for sub in subject:
+    print(f"Above average {sub}")
+    avg = scores[sub].mean()
+    print(scores[(scores[sub] > avg)].groupby("Gender").groups)
