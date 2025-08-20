8b5a7c8d502631c75d2e09e8fda953c2284d1300
Siddharth230 <siddharthlokhande04@gmail.com>
2025-08-20 13:19:08 +0530

    Introduction to Matplotlib library

--- WEEK 11/main.py
+++ WEEK 11/main.py
@@ -1,21 +1,60 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([5, 7, 8, 2, 17, 2, 9, 4, 11, 12, 9, 6])
-y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
-plt.scatter(x, y)
-plt.show()
+# x = np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
+# y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+# plt.scatter(x, y)
+# plt.savefig("scatter.png")
 
-x = np.array(["A", "B", "C", "D"])
+# x = np.array(["A", "B", "C", "D"])
+# y = np.array([3, 8, 1, 10])
+# plt.barh(x, y)
+# plt.savefig("barh.png")
+
+# x = np.random.normal(170, 10, 250)
+# plt.hist(x)
+# plt.savefig("hist.png")
+
+# y = np.array([35, 25, 25, 15])
+# mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+# plt.pie(y, labels=mylabels, startangle=90)
+# plt.savefig("pie.png")
+
+x = np.array([0, 1, 2, 3])
 y = np.array([3, 8, 1, 10])
-plt.bar(x, y)
-plt.show()
 
-x = np.random.normal(170, 10, 250)
-plt.hist(x)
-plt.show()
+plt.subplot(2, 3, 1)
+plt.plot(x, y)
 
-y = np.array([35, 25, 25, 15])
-mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
-plt.pie(y, labels=mylabels, startangle=90)
-plt.show()
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 2)
+plt.plot(x, y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 3)
+plt.plot(x, y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 4)
+plt.plot(x, y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 5)
+plt.plot(x, y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 6)
+plt.plot(x, y)
+
+plt.savefig("subplot.png")
